6f55b190521574c5ab1d325c911bcf58eb2c55ed
Kevin Kirsche <kevin.kirsche@verizon.com>
2018-12-14 15:47:06 -0500

    Initial Commit

---
+++ favorite-movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  favorite-movies
+//
+//  Created by Kevin Kirsche on 12/14/18.
+//  Copyright © 2018 Kevin Kirsche. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
